91ae194589fe7e136a789eb617ec92a08e29c9cb
aluthxander <aluthxander75@gmail.com>
2022-03-07 00:27:41 +0700

    membuat fungsi kalkulasi dan menambahkan pengkodisian serta merapikan kode program

--- kalkulator/main.py
+++ kalkulator/main.py
@@ -1,47 +1,49 @@
 # program kalkulator sederhana
 import os
+import time
+import re
+
+def kalkulasi(angka1, operator, angka2):
+    if operator == "+":
+        hasil = angka1+angka2
+    elif operator == "-":
+        hasil = angka1-angka2
+    elif operator == "/":
+        hasil = angka1/angka2
+    elif operator == '*' or operat.lower() == "x":
+        hasil = angka1*angka2
+    return hasil
+
+def iterasi():
+    jawab = str(input('\nketik exit untuk keluar dari program.\n'))
+    return jawab.lower()
 
 while True:
     try:
+        os.system('cls')
         print("kalkulator".center(30, '='))
         angka1 = int(input("Masukan angka 1\t\t: "))
         operat = str(input("Pilih operator(+,-,/,x)\t: "))
+        if not re.match(r'^[\+\-\*xX\/]', operat):
+            print('Warning: operator yang anda masukan salah')
+            time.sleep(2)
+            continue
         angka2 = int(input("Masukan angka 2\t\t: "))
         print('\nHasil:')
+        print(kalkulasi(angka1, operat, angka2))
 
-        if operat == "+":
-            hasil = angka1+angka2
-            print(angka1, '+', angka2, '=', hasil)
-        elif operat == "-":
-            hasil = angka1-angka2
-            print(angka1, '-', angka2, '=', hasil)
-        elif operat == "/":
-            hasil = angka1/angka2
-            print(angka1, '/', angka2, '=', hasil)
-        elif operat == '*' or operat.lower() == "x":
-            hasil = angka1*angka2
-            print(angka1, 'x', angka2, '=', hasil)
-        else:
-            print('Warning: operator yang anda masukan salah')
-            os.system('sleep(1000)')
-            os.system('cls')
-            continue
-
-        jawab = str(input('\nketik exit untuk keluar dari program.\n'))
-        if(jawab.lower() == 'exit'):
+        if(iterasi() == 'exit'):
             break
         else:
-            os.system('cls')
+            None
 
     except ValueError:
         print('\nWarning: input yang anda masukan bukan angka')
-        os.system('pause > null')
-        os.system('cls')
+        time.sleep(2)
     except ZeroDivisionError:
         print('\nHasil:')
         print(angka1, '/', angka2, '= 00')
-        jawab = str(input('\nketik exit untuk keluar dari program.\n'))
-        if(jawab.lower() == 'exit'):
+        if(iterasi() == 'exit'):
             break
         else:
-            os.system('cls')
+            None
