f1e23c27b52b12ac6b90e2ec7b68fa29cb1daaca
aluthxander <aluthxander75@gmail.com>
2022-03-12 23:31:09 +0700

    memodulasi program dengan memecah program menjadi beberapa fungsi

--- mencetak_deskripsi_data/main.py
+++ mencetak_deskripsi_data/main.py
@@ -1,34 +1,42 @@
+from logging import exception
 import pandas as pd
 import os
 import csv
+# G:\kuliah\machine Learning\probabilitas_statistik\package_python_data\pandas\kaggle_explorations-master\belajar_python_pandas\dataset\winemag-data-130k-v2.csv
+def cetak_csv(data, path_dir):
+    # deskripsi data
+    deskripsi = data.describe()
+    name_columns = data.columns
+    jml_kol = len(name_columns)
+    jml_baris = data[name_columns[0]].count()
+    Size_data = data.size
+    dic_data = {'Rows': jml_baris,
+                'Columns': jml_kol,
+                'Size': Size_data}
 
-df = pd.read_csv(
-    r"G:\kuliah\machine Learning\probabilitas_statistik\package_python_data\pandas\kaggle_explorations-master\belajar_python_pandas\dataset\winemag-data-130k-v2.csv")
+    # menulis data ke format csv
+    path_file = path_dir+r"\Deskripsi.csv"
+    deskripsi.to_csv(path_file)
+    with open(path_file, mode='a') as csv_file:
+        Writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for key, item in dic_data.items():
+            Writer.writerow([key, item])
+    print("Deskripsi data berhasil dibuat!")
 
-# path-path folder dan file
-path_current = os.getcwd()
-path_data = path_current+"\\mencetak_deskripsi_data\\deskripsi_data"
-path_file = path_data+"\\Deskripsi.csv"
+def persiapan_data():
+    # masukan path data dan nama file
+    path_in = input("Masukan path data beserta nama file: ")
+    df = pd.read_csv(path_in)
+    # path-path folder dan file
+    path_current = os.getcwd()
+    path_dir = path_current+r"\mencetak_deskripsi_data\deskripsi_data"
+    return df, path_dir
 
-# membuat direktori/folder baru untuk menampung file
-if not os.path.exists(path_data):
-    os.makedirs(path_data)
+def main():
+    df, path_dir = persiapan_data()
+    # membuat direktori/folder baru untuk menampung file
+    if not os.path.exists(path_dir):
+        os.makedirs(path_dir)
+    cetak_csv(df, path_dir)
 
-# deskripsi data
-deskripsi = df.describe()
-name_columns = df.columns
-jml_kol = len(name_columns)
-jml_baris = df[name_columns[0]].count()
-Size_data = df.size
-dic_data = {'Rows': jml_baris,
-            'Columns': jml_kol,
-            'Size': Size_data}
-
-# menulis data ke format csv
-deskripsi.to_csv(path_file)
-with open(path_file, mode='a') as csv_file:
-    Writer = csv.writer(csv_file, delimiter=',',
-                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for key, item in dic_data.items():
-        Writer.writerow([key, item])
-print("write done!")
+main()
