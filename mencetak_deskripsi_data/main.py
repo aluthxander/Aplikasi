db4ea8c9112f1cbf2a952ca97ae2ec4a5d05cf30
aluthxander <aluthxander75@gmail.com>
2022-03-12 12:00:23 +0700

    menambahkan baris di gitignore dan merapikan file /mencetak_deskripsi_data/main.py

--- mencetak_deskripsi_data/main.py
+++ mencetak_deskripsi_data/main.py
@@ -19,14 +19,15 @@ name_columns = df.columns
 jml_kol = len(name_columns)
 jml_baris = df[name_columns[0]].count()
 Size_data = df.size
-dic_data = {'Rows':jml_kol,
-            'Columns': jml_baris, 
+dic_data = {'Rows': jml_kol,
+            'Columns': jml_baris,
             'Size': Size_data}
 
 # menulis data ke format csv
 deskripsi.to_csv(path_file)
 with open(path_file, mode='a') as csv_file:
-    Writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    Writer = csv.writer(csv_file, delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for key, item in dic_data.items():
-        Writer.writerow([key,item])
+        Writer.writerow([key, item])
 print("write done!")
